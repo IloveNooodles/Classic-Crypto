fd911c8a3527c9a6bfdda92dca015f534855f1e9
IloveNooodles <mgarebaldhie80@gmail.com>
2023-01-25 20:33:54 +0700

    Feat(frontend): adding home layout and page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,91 +1,43 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from "@next/font/google";
+import Link from "next/link";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+const listOfCipher: string[] = [
+  "Vigenere Cipher",
+  "Auto-key Vigenere Cipher",
+  "Extended Vigenere Cipher",
+  "Affine Cipher",
+  "Playfair Cipher",
+  "Hill Cipher",
+];
+const slug: string[] = [
+  "vignere-cipher",
+  "auto-key-vignere-cipher",
+  "extended-vignere-cipher",
+  "affine-cipher",
+];
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        <code className={styles.title}>
+          Classical Cipher <br />
+          Encryptor, Decryptor, Solver
+        </code>
       </div>
-
       <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {listOfCipher.map((cipher: string, index: number) => {
+          let slug = cipher.toLowerCase().replace(" ", "-");
+          return (
+            <Link href={`/${slug}`} className={styles.card} key={index}>
+              <h2 className={inter.className}>{cipher}</h2>
+              <p className={inter.className}>Cipher test</p>
+            </Link>
+          );
+        })}
       </div>
     </main>
-  )
+  );
 }
