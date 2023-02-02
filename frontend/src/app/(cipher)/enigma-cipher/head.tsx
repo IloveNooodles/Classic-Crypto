6865d6b6a990b28054c828f06d6b03dfe724e6f4
IloveNooodles <mgarebaldhie80@gmail.com>
2023-02-02 16:42:59 +0700

    feat: remove api on next and enigma cipher

--- frontend/src/app/(cipher)/enigma-cipher/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Classic Cryptography - Extended Vignere Cipher</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Classic Cryptography solver" />
-    </>
-  );
-}
